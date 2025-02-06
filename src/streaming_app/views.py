d6256cee8c5c9ceb2d9eeab062e678fce1419dda
Emily Harris <emily.harris@kraken.tech>
2025-02-06 10:04:29 +0000

    song asre now vivibles from list songs

--- src/streaming_app/views.py
+++ src/streaming_app/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 
+from . import models
+
 
 # Create your views here.
 class HomeView(generic.TemplateView):
@@ -10,3 +12,25 @@ class HomeView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context["title"] = "Home"
         return context
+
+
+class ListAllSongs(generic.ListView):
+    template_name = "list_of_songs.html"
+    title = "list of songs"
+    model = models.Song
+    fields = [
+        field
+        for field in models.Song._meta.get_fields()
+        if field.verbose_name != "meta data"
+    ]
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context["title"] = self.title
+        context["model"] = self.model.objects.all()
+        context["fields"] = self.fields
+        return context
+
+
+class SongMetaDataView(generic.DetailView):
+    pass
