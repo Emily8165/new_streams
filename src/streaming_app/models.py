754f4b61ca298f78134bda6fc646f2f346e49bde
Emily Harris <emily.harris@kraken.tech>
2025-03-10 17:00:09 +0000

    working branch

--- src/streaming_app/models.py
+++ src/streaming_app/models.py
@@ -2,11 +2,13 @@ import csv
 import datetime
 import json
 
+import numpy as np
+import requests
 from django.db import models
 from django.utils import timezone
 from mutagen.mp3 import MP3
+from PIL import Image
 
-colours = (("red", "red"), ("blue", "blue"), ("green", "green"))
 track = (("ALBUM", "ALBUM"), ("EP", "EP"), ("LP", "LP"), ("SINGLE", "SINGLE"))
 
 
@@ -28,9 +30,13 @@ def calculate_song_length(file: str) -> str:
 
 
 class ArtistMetaData(models.Model):
-    number_of_songs = models.PositiveIntegerField(default=0)
     genres = models.CharField(max_length=20, choices=get_music_genres())
     country = models.CharField(max_length=255)
+    artist_key = models.CharField(max_length=25)
+
+    @property
+    def number_of_songs(self) -> int:
+        return 
 
 
 class Artist(models.Model):
@@ -40,17 +46,49 @@ class Artist(models.Model):
 
 class SongMetaData(models.Model):
     artists = models.ForeignKey(Artist, on_delete=models.CASCADE, default=1)
-    featured_artists = models.CharField(max_length=255, null=True)
     record_label = models.CharField(max_length=50, default="Independent")
     language = models.CharField(max_length=10, choices=get_languages())
     is_cover = models.BooleanField(default=False)
     release_date = models.DateField()
     genre = models.CharField(max_length=20, choices=get_music_genres())
     lyrics = models.TextField(null=True, blank=True)
-    main_artwork_colour = models.CharField(max_length=5, choices=colours)
-    BPM = models.PositiveIntegerField()
-    main_instruments = models.CharField(max_length=255)
+    main_artwork_colour = models.CharField(max_length=12)
+    main_instruments = models.CharField(max_length=255, null=True)
     track_type = models.CharField(max_length=6, choices=track)
+    spotify_ref = models.CharField(max_length=32, default=1)
+
+    @staticmethod
+    def new(
+        cls,
+        artist: Artist,
+        featured_artists: Artist,
+        record_label: str,
+        language: str,
+        is_cover: bool,
+        release_date: datetime.date,
+        genre: str,
+        lyrics: str,
+        main_artwork_colour: str,
+        BPM: int,
+        main_instruments: str,
+        track_type: str,
+        spotify_ref: str,
+    ):
+        return SongMetaData.objects.create(
+            artist=artist,
+            featured_artists=featured_artists,
+            record_label=record_label,
+            language=language,
+            is_cover=is_cover,
+            release_date=release_date,
+            genre=genre,
+            lyrics=lyrics,
+            main_artwork_colour=main_artwork_colour,
+            BPM=BPM,
+            main_instruments=main_instruments,
+            track_type=track_type,
+            spotify_ref=spotify_ref,
+        )
 
     @property
     def song_length(self, song_name: str) -> str:
@@ -59,6 +97,18 @@ class SongMetaData(models.Model):
     @property
     def song_age(self) -> datetime.timedelta:
         return datetime.datetime.now() - self.release_date
+
+    @property
+    def BPM(self) -> int:
+        return 0
+
+    @property
+    def artwork_colour(self, image_url) -> str:
+        i = Image.open(requests.get(image_url, stream=True).raw)
+        np_image = np.array(i)
+        average_color_array = np_image.mean(axis=(0, 1))
+        average_color = tuple(map(int, average_color_array))
+        return average_color
 
 
 class Song(models.Model):
